98edc6b4c4fb27cf8fa5386d8abe3768158ac08a
Juan Orellana <cefa68000@gmail.com>
2018-05-17 10:11:37 +0200

    First commit

---
+++ Optimus/Optimus.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimus
+{
+    public class Optimus
+    {
+        public ulong Prime { get; set; }
+        public ulong ModInverse { get; set; }
+        public ulong Random { get; set; }
+        //Does not check if prime is actually a prime number but does not work correctly if it not a prime number
+        public Optimus(ulong prime, ulong modInverse, ulong random)
+        {
+            Prime = prime;
+            ModInverse = modInverse;
+            Random = random;
+        }
+
+        public uint Encode(uint number)
+        {
+            var encoded = ((number * Prime) & int.MaxValue) ^ Random;
+            return (uint)encoded;
+        }
+
+        public uint Decode(uint number)
+        {
+            var decoded = ((number ^ Random) * ModInverse) & int.MaxValue;
+            return (uint)decoded;
+        }
+
+        public long GetModInverseOfPrime(ulong prime)
+        {
+            var max = ((long)int.MaxValue + 1);
+            var inverse = GetModInverse((long)prime, max);
+            return inverse;
+        }
+
+        public long GetModInverse(long a, long n)
+        {
+            long i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                long t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
+        }
+    }
+}
---
+++ Optimus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var optimus = new Optimus(2123809381, 1885413229, 146808189);
+            var number = 15u;
+            Console.WriteLine("OriginalValue: " + number);
+            var newId = optimus.Encode(number);
+            Console.WriteLine("Encode: " + newId);
+            Console.WriteLine("Decode: " + optimus.Decode(newId));
+
+            //Console.WriteLine("Modinverse: " + optimus.GetModInverseOfPrime(2123809381));
+        }
+    }
+}
